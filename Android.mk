182e4271e9549b7cdc13a5fe799257326cf4df79
XpLoDWilD <xplod@ouverta.fr>
2013-08-24 15:51:56 +0200

    Add missing files

---
+++ Android.mk
@@ -0,0 +1,3 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(LOCAL_PATH)/main/src/main/Android.mk
---
+++ main/src/main/src/com/google/android/apps/dashclock/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.google.android.apps.dashclock;
+
+public class BuildConfig {
+	public final static boolean DEBUG = false;
+}
